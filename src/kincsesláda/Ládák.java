00fe53a5db3a58d8f94b7d0e2d97241d9a8a5817
hallgato hallgato <hallgato@oktatas.local>
2021-09-10 15:56:07 +0200

    Létrehoz

---
+++ src/kincsesláda/Ládák.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kincsesláda;
+
+/**
+ *
+ * @author hallgato
+ */
+public class Ládák {
+    private String nev;
+    private String mondas;
+    
+    Ládák(String nev, String mondas){
+    this.nev=nev;
+    this.mondas=mondas;
+    }
+    
+    
+}
